c59770ef81924c7962049dd35f62df40e40db928
ankan17 <friendyankan@gmail.com>
2018-01-14 15:14:12 +0530

    Create the UI for registration
    Create a page for registration to hit the register endpoint
    of the backend and get a JSON response in return.

---
+++ client/src/components/Register.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>User Registration</h1>
+    <input
+      type="text"
+      name="username"
+      v-model="username"
+      placeholder="Enter your github username" />
+    <br>
+    <br>
+    <input type="text"
+    name="registration_year"
+    v-model="registration_year"
+    placeholder="Enter your year of registration in the college" />
+    <br>
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import Registration from '@/services/Registration'
+export default {
+  data () {
+    return {
+      username: '',
+      registration_year: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await Registration.register({
+        username: this.username,
+        registration_year: this.registration_year
+      })
+      console.log(response.data)
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
